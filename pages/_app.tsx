aeff165e69053de24df5815f7ceba9f94f356965
Weston Nelson <dogooddowell@protonmail.com>
2023-06-06 17:16:34 -0500

    Update _app.tsx
    Correct _app configuration.
    
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,16 @@
-import type { NextPage } from "next";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-const Home: NextPage = () => {
-  const router = useRouter();
+// This is the chain your dApp will work on.
+const activeChain = "arbitrum";
 
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.container}>
-      {/* Top Section */}
-      <h1 className={styles.h1}>NFTEarth Staking Rewards</h1>
-      <div className={styles.nftBoxGrid}>
-        <div
-          className={styles.optionSelectBox}
-          role="button"
-          onClick={() => router.push(`/mint`)}
-        >
-          {/* Mint a new NFT */}
-          <Image src="/icons/drop.svg" alt="drop" width={64} height={64} />
-          <h2 className={styles.selectBoxTitle}>Need an Earthling? Mint one here.</h2>
-          <p className={styles.selectBoxDescription}>
-            Mint an Earthling here.
-          </p>
-        </div>
-
-        <div
-          className={styles.optionSelectBox}
-          role="button"
-          onClick={() => router.push(`/stake`)}
-        >
-          {/* Staking an NFT */}
-          <Image src="/icons/token.svg" alt="token" width={64} height={64} />
-          <h2 className={styles.selectBoxTitle}>Stake Your Earthling NFT</h2>
-          <p className={styles.selectBoxDescription}>
-            Stake your Earthling NFT for juicy NFTE rewards to enjoy the start of NFTFi summer on Arbitrum.
-          </p>
-        </div>
-      </div>
-    </div>
+    <ThirdwebProvider activeChain={activeChain}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
   );
-};
+}
 
-export default Home;
+export default MyApp;
